a142abebcb29d759a34d9959b0a3ac97105b4390
Jen <124745888+jenstem@users.noreply.github.com>
2024-03-12 16:01:45 -0400

    Find auction data and append to list

--- main.py
+++ main.py
@@ -22,3 +22,15 @@ for i in header:
     titles.append(title)
 
 df = pd.DataFrame(columns = titles)
+
+# Details of the auction
+rows = table.find_all("tr")
+print(rows)
+
+for i in rows[1:]:
+    first_td = i.find_all("td")[0].find("div",class_ = "ih-pt-ic").text.strip()
+    data = i.find_all("td")[1:]
+    row = [tr.text for tr in data]
+    row.insert(0,first_td)
+    l = len(df)
+    df.loc[l] = row
